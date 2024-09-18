b5149e950788d7d322cefd68ee891d9672b40a06
Nisibl <eliceybobrov@mail.ru>
2024-09-18 10:51:00 +0700

    transferring the program to the repository
    creating a repository and transferring the program to the repository

---
+++ InfoSystemPtactika1/InfoSystemPtactika1/ProductInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoSystemPtactika1
+{
+    internal class ProductInfo
+    {
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public static ProductInfo CreateProductFromString(string input)
+        {
+
+            string propertiesString = input.Substring(input.IndexOf(":") + 1).Trim();
+
+            string[] properties = propertiesString.Split(new[] { "  " }, StringSplitOptions.RemoveEmptyEntries);
+
+            ProductInfo product = new ProductInfo
+            {
+                Date = DateTime.ParseExact(properties[0], "yyyy.MM.dd", null),
+                Name = properties[1].Trim('"'),
+                Quantity = int.Parse(properties[2])
+            };
+
+            return product;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("Поступление товаров");
+            Console.WriteLine("Дата: " + Date);
+            Console.WriteLine("Название: " + Name);
+            Console.WriteLine("Количество: " + Quantity);
+            
+        }
+    }
+}
---
+++ InfoSystemPtactika1/InfoSystemPtactika1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InfoSystemPtactika1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string input = "Поступление товара:  2023.04.15  \"Футболка\"  10";
+
+            ProductInfo product = ProductInfo.CreateProductFromString(input);
+            
+            product.PrintInfo();
+
+        }
+    }
+}
